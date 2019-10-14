9419031559c9ba0c603c2daf7babc65ebed00d7d
Walmor <walmorfelix@hotmail.com>
2019-10-14 18:50:49 -0300

    Criação das Entidades

---
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using QuickBuy.Dominio.Entidades;
+using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Contratos
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+
+        /// <summary>
+        /// Um usuário pode ter nenhum ou muitos pedidos
+        /// </summary>
+        public ICollection<Pedido> Pedidos { get; set; }
+
+
+    }
+}
